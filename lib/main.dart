a888abc7afc3f224deaeb5bb7ab4d13358336426
Md Rakibull Haque <rakibnirob100@gmail.com>
2023-08-28 23:58:07 +0600

    Modify

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   GoogleMapController? _controller;
   final List<LatLng> _polylineCoordinates = [];
   final Location _location = Location();
-  Set<Marker> _markers = {}; // Add this line
+  Set<Marker> _markers = {};
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         },
         markers: _markers,
       ),
-      // If clicked on the floating action button, then get the current location
+
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(45.0),
         child: Row(
@@ -95,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
     bool serviceEnabled;
     PermissionStatus permission;
 
-    // Check if location services are enabled.
+
     serviceEnabled = await _location.serviceEnabled();
     if (!serviceEnabled) {
       serviceEnabled = await _location.requestService();
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     }
 
-    // Request location permission if not granted.
+
     permission = await _location.hasPermission();
     if (permission == PermissionStatus.denied) {
       permission = await _location.requestPermission();
@@ -113,25 +113,22 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     }
 
-
-
-
   }
 
 
   Future<void> _getLocationAndDrawPolyline() async {
-    await _centerMapOnLocation(); // Call the permission method before location updates
+    await _centerMapOnLocation();
 
     _location.onLocationChanged.listen((locationData) {
       LatLng latLng = LatLng(locationData.latitude!, locationData.longitude!);
       setState(() {
         _polylineCoordinates.add(latLng);
 
-        // Update the marker's position
-        _markers.clear(); // Clear the previous markers
+
+        _markers.clear();
         _markers.add(
           Marker(
-            markerId: MarkerId('marker1'),
+            markerId: const MarkerId('marker1'),
             position: latLng,
             icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
             infoWindow:  InfoWindow(title: 'My current location $latLng'),
